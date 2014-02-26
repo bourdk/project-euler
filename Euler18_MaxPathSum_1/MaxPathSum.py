06b22b25afb3d53d2055306d59d8210a13e29664
bourdk <pavel.bourdykine@gmail.com>
2014-02-26 22:55:02 +0000

    prob 18 and 67 solutions

--- Euler18_MaxPathSum_1/MaxPathSum.py
+++ Euler18_MaxPathSum_1/MaxPathSum.py
@@ -1,8 +1,13 @@
-filePath = 'test1'
+filePath = 'triangle.txt'
 
 fp = open(filePath, 'r')
+nums = [list(map(int, line.split())) for line in fp]
+fp.close()
 
-for line in fp:
-    a = line.split()
-    
-    print(a)
+for i in reversed(range(1, len(nums))):
+    for j in range(len(nums[i])-1):
+        # choose max of 2 children and 'fold' it into the parent
+        m = max(nums[i][j], nums[i][j+1])
+        nums[i-1][j] += m
+
+print(nums[0][0])
