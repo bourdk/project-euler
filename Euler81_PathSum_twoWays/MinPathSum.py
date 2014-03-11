a228f0296f322eadda51316eacdd6cc76158df06
bourdk <pavel.bourdykine@gmail.com>
2014-03-11 10:27:46 -0700

    index calcs

--- Euler81_PathSum_twoWays/MinPathSum.py
+++ Euler81_PathSum_twoWays/MinPathSum.py
@@ -12,8 +12,18 @@ for i in range(len(nums)):
     for j in range(len(nums[i])):
         r_min = min(nums[i][j + 1:])
         d_min = min(numsT[j][i + 1:])
+        print("r_min: ", r_min)
+        print("d_min: ", d_min)
         r_index = nums[i][j + 1:].index(r_min)
         d_index = numsT[j][i + 1:].index(d_min)
+        print("r_index: ", r_index)
+        print("d_index: ", d_index)
+
+        print("r_min_cost: ", nums[i][r_index:])
+        print("d_min_cost: ", numsT[j][d_index:])
+#         r_min_cost = sum(nums[j:r_index])
+#         d_min_cost = sum(numsT[i:d_index])
+#         print(min(r_min_cost, d_min_cost))
 
 
 
